ba1567f030dafecf4c48747a4ba7a68469b39f58
Parth Narkhede <parth.narkhede@centralogic.net>
2024-04-19 13:02:32 +0530

    structure

---
+++ lib/modules/dashboard/presentation/view/desktop_view.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Row(
+        children: [
+          Container(
+            width: 100,
+            height: 100,
+            color: Colors.amber,
+          )
+        ],
+      ),
+    );
+  }
+}
